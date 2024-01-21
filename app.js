f864d8ecdb41604fff82edff5f18e24148b68212
Fernando Avendaño Hernandez <fernando.softdesigner@gmail.com>
2024-01-21 08:01:11 -0600

    agregue loop while para que el juego pida los numero infinitamente

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 //Variables
 let numeroSecreto = 4;
-let numeroUsuario = prompt("Me indicas un número por favor:");
+let numeroUsuario = 0;
+while (numeroUsuario != numeroSecreto) {
+    let numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
 
-console.log(numeroUsuario);
-/*Este codigo realiza la comparació*/
+    console.log(numeroUsuario);
+    /*Este codigo realiza la comparació*/
 
-if (numeroUsuario == numeroSecreto) {
-    //Acertamos la condición
-    alert(`Acertaste, el número es : ${numeroUsuario}`);
-} else{
-    if (numeroUsuario > numeroSecreto) {
-        alert('El número secreto es menor');
-    } else {
-        alert('El numero secreto es mayor');
+    if (numeroUsuario == numeroSecreto) {
+        //Acertamos la condición
+        alert(`Acertaste, el número es : ${numeroUsuario}`);
+    } else{
+        if (numeroUsuario > numeroSecreto) {
+            alert('El número secreto es menor');
+        } else {
+            alert('El numero secreto es mayor');
+        }
+        //No Acertamos la condición
+        alert('Lo siento, no acertaste el número');
     }
-    //No Acertamos la condición
-    alert('Lo siento, no acertaste el número');
 }
-
