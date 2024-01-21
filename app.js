5260746d4ba121de7b77a66a621a08fe91dc3cc1
Fernando Avendaño Hernandez <fernando.softdesigner@gmail.com>
2024-01-21 06:50:40 -0600

    complete el if y añadi comentarios

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
+//Variables
 let numeroSecreto = 6;
 let numeroUsuario = prompt("Me indicas un número por favor:");
 
 console.log(numeroUsuario);
+/*Este codigo realiza la comparació*/
 
 if (numeroUsuario == numeroSecreto) {
+    //Acertamos la condición
     alert('Acertaste el número');
+} else{
+    //No Acertamos la condición
+    alert('Lo siento, no acertaste el número');
 }
+
