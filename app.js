f04cec11c457a310ac06546523becd77784c3be6
Fernando Avendaño Hernandez <fernando.softdesigner@gmail.com>
2024-01-21 17:34:15 -0600

    cambiamos la estructura del contador y utilizamos un operdor ternario para vez y veces

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 let numeroSecreto = 4;
 let numeroUsuario = 0;
 let intentos = 1;
-let palabraVeces = 'vez';
+//let palabraVeces = 'vez';
+let maximosIntentos= 2;
 while (numeroUsuario != numeroSecreto) {
     let numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
 
@@ -11,7 +12,7 @@ while (numeroUsuario != numeroSecreto) {
 
     if (numeroUsuario == numeroSecreto) {
         //Acertamos la condición
-        alert(`Acertaste, el número es : ${numeroUsuario}. Lo hiciste en ${intentos} ${palabraVeces}`);
+        alert(`Acertaste, el número es : ${numeroUsuario}. Lo hiciste en ${intentos} ${intentos ==1 ? 'vez' : 'veces'}`);
     } else{
         if (numeroUsuario > numeroSecreto) {
             alert('El número secreto es menor');
@@ -19,10 +20,12 @@ while (numeroUsuario != numeroSecreto) {
             alert('El numero secreto es mayor');
         }
         //Incrementamos el contador cuando no acierta
-        intentos = intentos + 1;
-        palabraVeces = 'veces';
+        //intentos = intentos + 1;
+        //intentos += 1;
+        intentos++;
+       // palabraVeces = 'veces';
         if (intentos > 3) {
-            alert('Llegaste al número maximo de intentos');
+            alert(`Llegaste al número maximo de ${maximosIntentos} intentos`);
             break;
         }
         //No Acertamos la condición
