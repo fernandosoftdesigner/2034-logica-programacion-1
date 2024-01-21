87eacc9b89efdcd512dc908b0b49a7ddc4c9f80a
Fernando Avendaño Hernandez <fernando.softdesigner@gmail.com>
2024-01-21 08:26:53 -0600

    agregue contador de veces que utilizaste para adivinar numero

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 //Variables
 let numeroSecreto = 4;
 let numeroUsuario = 0;
+let intentos = 1;
+let palabraVeces = 'vez';
 while (numeroUsuario != numeroSecreto) {
     let numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
 
@@ -9,14 +11,17 @@ while (numeroUsuario != numeroSecreto) {
 
     if (numeroUsuario == numeroSecreto) {
         //Acertamos la condición
-        alert(`Acertaste, el número es : ${numeroUsuario}`);
+        alert(`Acertaste, el número es : ${numeroUsuario}. Lo hiciste en ${intentos} ${palabraVeces}`);
     } else{
         if (numeroUsuario > numeroSecreto) {
             alert('El número secreto es menor');
         } else {
             alert('El numero secreto es mayor');
         }
+        //Incrementamos el contador cuando no acierta
+        intentos = intentos + 1;
+        palabraVeces = 'veces';
         //No Acertamos la condición
-        alert('Lo siento, no acertaste el número');
+        //alert('Lo siento, no acertaste el número');
     }
 }
