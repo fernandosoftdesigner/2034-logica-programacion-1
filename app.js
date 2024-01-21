561d542612ee1297b108deedcf97333736661b25
Fernando Avendaño Hernandez <fernando.softdesigner@gmail.com>
2024-01-21 07:50:28 -0600

    agregue para avisar al usuario si el numero es mayor o menor

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //Variables
-let numeroSecreto = 6;
+let numeroSecreto = 4;
 let numeroUsuario = prompt("Me indicas un número por favor:");
 
 console.log(numeroUsuario);
@@ -9,6 +9,11 @@ if (numeroUsuario == numeroSecreto) {
     //Acertamos la condición
     alert(`Acertaste, el número es : ${numeroUsuario}`);
 } else{
+    if (numeroUsuario > numeroSecreto) {
+        alert('El número secreto es menor');
+    } else {
+        alert('El numero secreto es mayor');
+    }
     //No Acertamos la condición
     alert('Lo siento, no acertaste el número');
 }
